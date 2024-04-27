13bc553f7c31282133fd3bacee77ef241b821931
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2024-04-27 02:06:11 -0700

    added gitignore to build file

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.purpleAccent,
           title: const Text('TEST APP'),
         ),
       ),
